c45aa20eedb25c304d42fa1c3548e085ad2f184c
A11y Up <a11yup+github@posteo.de>
2022-02-07 18:42:12 +0100

    Add unit tests for transform function

---
+++ src/transform.test.js
@@ -0,0 +1,49 @@
+import transform from "./transform.js";
+
+it("win in first round", () => {
+  const input = `🟩🟩🟩🟩🟩\n`;
+  const result = transform(input);
+
+  const targetResult = `Zeile 1: GG!\n`;
+
+  console.log(result);
+  console.log(targetResult);
+
+  expect(result).toBe(targetResult);
+});
+
+it("win in last round", () => {
+  const input = `⬜⬜🟨⬜🟨\n🟨⬜🟨🟨⬜\n⬜⬜🟩🟩🟨\n⬜🟩🟩🟩🟨\n🟩🟩🟩🟩🟩`;
+  const result = transform(input);
+
+  const targetResult = `Zeile 1: 3. und 5. an falscher Stelle\nZeile 2: 1. und 3. und 4. an falscher Stelle\nZeile 3: 3. und 4. korrekt, 5. an falscher Stelle\nZeile 4: 2. und 3. und 4. korrekt, 5. an falscher Stelle\nZeile 5: GG!\n`;
+
+  console.log(result);
+  console.log(targetResult);
+
+  expect(result).toBe(targetResult);
+});
+
+it("win in 4th round", () => {
+  const input = `⬜⬜🟨⬜🟨\n🟨⬜🟨🟨⬜\n⬜⬜🟩🟩🟨\n🟩🟩🟩🟩🟩`;
+  const result = transform(input);
+
+  const targetResult = `Zeile 1: 3. und 5. an falscher Stelle\nZeile 2: 1. und 3. und 4. an falscher Stelle\nZeile 3: 3. und 4. korrekt, 5. an falscher Stelle\nZeile 4: GG!\n`;
+
+  console.log(result);
+  console.log(targetResult);
+
+  expect(result).toBe(targetResult);
+});
+
+it("lose completely", () => {
+  const input = `⬜⬜⬜⬜⬜\n⬜⬜⬜⬜⬜\n⬜⬜⬜⬜⬜\n⬜⬜⬜⬜⬜\n⬜⬜⬜⬜⬜`;
+  const result = transform(input);
+
+  const targetResult = `Zeile 1: ZONK!\nZeile 2: ZONK!\nZeile 3: ZONK!\nZeile 4: ZONK!\nZeile 5: ZONK!\n`;
+
+  console.log(result);
+  console.log(targetResult);
+
+  expect(result).toBe(targetResult);
+});
