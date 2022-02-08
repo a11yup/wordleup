f4326a5aa0270603de5e0961b5a628daff629f72
A11y Up <a11yup+github@posteo.de>
2022-02-08 17:02:50 +0100

    Make Transformation happen instantly + add error message to UI

--- src/App.js
+++ src/App.js
@@ -6,27 +6,40 @@ function App() {
   const textAreaElement = createRef();
 
   const [result, setResult] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
 
-  const handleInput = () => {
-    const transformedResult = transform(textAreaElement.current.value);
-    setResult(transformedResult);
+  const handleInputChange = (event) => {
+    let transformedResult;
+    try {
+      transformedResult = transform(event.target.value);
+      setResult(transformedResult);
+      setErrorMessage("");
+    } catch (error) {
+      setResult("");
+      setErrorMessage(error.message);
+    }
   };
 
   return (
     <div className="App">
       <header className="App-header">
-        <h1>WORDLE UP!!!</h1>
+        <h1>WORDLE UP!</h1>
+        <p>
+          Ein Konverter für Wordle Emoji Matrix in reinen Text (für mehr
+          Accessibility in Wordle Tweets)
+        </p>
 
-        <h2>INPUT</h2>
-        <label htmlFor="input">Worlde Emoji Matrix Input:</label>
+        <h2>Eingabe</h2>
+        <label htmlFor="input">Worlde Emoji Matrix:</label>
         <textarea
           id="input"
           className="emoji-matrix-input"
           ref={textAreaElement}
+          onChange={handleInputChange}
         ></textarea>
-        <button onClick={handleInput}>TRANSFORM!</button>
+        <p className="error-message">{errorMessage}</p>
 
-        <h2>OUTPUT</h2>
+        <h2>Resultat</h2>
         <p className="result">{result}</p>
       </header>
     </div>
